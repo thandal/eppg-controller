23a387d91b8e3ce7b4a03d32d679cabf093fbbf4
thandal <than@timbrel.org>
2023-06-13 20:34:56 -0400

    Clarify CRC code

--- lib/crc.c
+++ lib/crc.c
@@ -11,7 +11,6 @@
 uint16_t crc16(uint8_t *buf, uint32_t size) {
   uint16_t crc = 0;
   for (uint32_t i = 0; i < size; i++)
-    crc = (crc << 8) ^ crc16table[buf[i] ^ crc>>8];
+    crc = (crc << 8) ^ crc16table[buf[i] ^ (crc >> 8)];
   return crc;
 }
-
